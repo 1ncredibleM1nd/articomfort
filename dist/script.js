27690d2873425c0212c6ae172401a36835c68bb7
Андрей Родин <a.rodin@avsw.local>
2021-05-17 15:11:49 +0300

    Finished desktop version, but carousel isn't working now

--- dist/script.js
+++ dist/script.js
@@ -93,7 +93,16 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
 
 /***/ })
 
